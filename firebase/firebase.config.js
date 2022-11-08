bb15b2320f481c9756aad34d096f883288740497
Carlosfromsubway <tlee285@my.bcit.ca>
2022-11-08 12:57:50 -0800

    update

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC6qC9D9-6r3uw_RJv7lkX8P0X3qIuup1M",
-  authDomain: "digital-asset-5b107.firebaseapp.com",
-  projectId: "digital-asset-5b107",
-  storageBucket: "digital-asset-5b107.appspot.com",
-  messagingSenderId: "816088038588",
-  appId: "1:816088038588:web:4293032169a0d4ce8964b9"
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId
+
 };
 
 const app = initializeApp(firebaseConfig);
